54eddcb2b2bcb40b5e83f8461f1f9c7baee7f13f
Ishaan Kumar <ffishaan@gmail.com>
2021-03-07 22:26:41 -0500

    add generation code

--- models/prediction.py
+++ models/prediction.py
@@ -5,6 +5,7 @@ import torch
 import torch.nn as nn
 from constants import BATCH_FIRST
 from torch.distributions.multivariate_normal import MultivariateNormal
+from torch.distributions.categorical import Categorical
 from torch.nn.utils.rnn import pad_packed_sequence
 import numpy as np
 
@@ -45,10 +46,7 @@ class PredModel(nn.Module):
         y_hat, (self.h_n, self.c_n) = self.lstm(x)
         return y_hat
 
-    def infer(self, lstm_out, x, mask):
-        lstm_out, _ = pad_packed_sequence(lstm_out, batch_first=BATCH_FIRST)
-        x, _ = pad_packed_sequence(x, batch_first=BATCH_FIRST)
-        lstm_out = lstm_out[:, :-1, :]  # discard last one
+    def _process_output(self, lstm_out):
         e_t = lstm_out[..., 0]
         mp = lstm_out[..., 1:]
         ws, means, log_std, corr = torch.split(mp, self.split_sizes, dim=-1)
@@ -62,6 +60,42 @@ class PredModel(nn.Module):
         covariance_mat = torch.cat(
             [std_1, std_1 * std_2 * corr, std_1 * std_2 * corr, std_2], axis=-1
         ).view(b, s, self.num_mixtures, 2, 2)
+
+        return ws, means, covariance_mat, e_t
+
+    @torch.no_grad()
+    def generate(self, device):
+        inp = torch.zeros(2, device=device).unsqueeze(0).unsqueeze(0)
+        h_n, c_n = torch.zeros(self.num_layers, 1, self.output_dim), torch.zeros(
+            self.num_layers, 1, self.output_dim
+        )
+        out = []
+        for _ in range(700):
+            y_hat, (h_n, c_n) = self.lstm(inp, (h_n, c_n))
+
+            ws, means, covariance_mat, e_t = self._process_output(y_hat)
+
+            ws = ws.squeeze().exp()
+
+            dist = Categorical(probs=ws)
+            j = dist.sample()
+            dist = MultivariateNormal(
+                means[..., j, :], covariance_matrix=covariance_mat[..., j, :, :]
+            )
+            x_nt = dist.sample()
+            x_nt = x_nt.squeeze(0)
+            e_t = e_t.sigmoid()
+            e_t[e_t > 0.5] = 1
+            e_t[e_t <= 0.5] = 0
+            out.append(torch.cat([e_t, x_nt], axis=1))
+        return out
+
+    def infer(self, lstm_out, x, mask):
+        lstm_out, _ = pad_packed_sequence(lstm_out, batch_first=BATCH_FIRST)
+        x, _ = pad_packed_sequence(x, batch_first=BATCH_FIRST)
+        lstm_out = lstm_out[:, :-1, :]  # discard last one
+
+        ws, means, covariance_mat, e_t = self._process_output(lstm_out)
 
         # apply mask
         # new_mask = mask[:, :-1]
