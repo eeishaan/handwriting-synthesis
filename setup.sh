54255ddeaee3162f87c0420394dbedaa786d180f
Ishaan Kumar <ffishaan@gmail.com>
2021-03-09 23:00:55 -0500

    strict setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
-module load cuda/10.2 python/3.7
-cp -r data/ /tmp/
-virtualenv --no-download /tmp/x
-source /tmp/x/bin/activate
-pip install --no-index -r requirements.txt
+module load cuda/10.2 python/3.7 && \
+cp -r data/ /tmp/ && \
+virtualenv --no-download /tmp/x && \
+source /tmp/x/bin/activate && \
+pip install --no-index -r requirements.txt 
