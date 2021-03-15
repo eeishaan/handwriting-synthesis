4cdfd7bd865036dd08508cc0e931d07f457889c5
Ishaan Kumar <ffishaan@gmail.com>
2021-03-15 00:07:11 -0400

    modify train script for 2nd stage

--- train.py
+++ train.py
@@ -1,10 +1,13 @@
 import enum
 import os
 import pathlib
-import time
 import shutil
+import time
+
+import fire
 import torch
 from torch.nn.functional import binary_cross_entropy, binary_cross_entropy_with_logits
+from torch.nn.utils import clip_grad_norm_
 from tqdm import tqdm
 
 from constants import BATCH_FIRST
@@ -13,18 +16,22 @@ from models.prediction import PredModel
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 root = "/tmp/" if torch.cuda.is_available() else ""
-from torch.nn.utils import clip_grad_norm_
 
 
-def train():
+def train(second_stage: bool = False):
     batch_size = 64
     lr = 1e-4
-    model = PredModel(3, 3, 20, batch_size=batch_size, hidden_dim=400).to(device)
+    with_texts = True if second_stage else False
+    model = PredModel(
+        3, 3, 20, batch_size=batch_size, hidden_dim=400, with_texts=with_texts
+    ).to(device)
     optim = torch.optim.AdamW(model.parameters(), lr=lr, weight_decay=1e-5)
     # optim = torch.optim.RMSprop(
     #     model.parameters(), lr=lr, alpha=0.9, momentum=0.95, eps=1e-4
     # )
-    loader, dataset = get_loader(root + "data/strokes-py3.npy", batch_size)
+    loader, dataset = get_loader(
+        root + "data/strokes-py3.npy", batch_size, with_texts=with_texts
+    )
     # loader, dataset = get_loader("data/strokes-py3.npy", batch_size)
     num_epochs = 100
     save_dir = f"{root}runs/{batch_size}_{lr}_{time.time()}"
@@ -32,13 +39,14 @@ def train():
     bptt_steps = 100
     # model.generate(device)
 
-    def _single_step(x, labels, label_mask, input_mask):
+    def _single_step(x, labels, label_mask, input_mask, seqs=None):
         x = x.to(device)
         labels = labels[:, 1:].to(device)
         label_mask = label_mask[:, 1:].to(device)
         input_mask = input_mask[:, :-1].to(device)
 
-        out = model(x[:, :-1, :])
+        out = model(x[:, :-1, :], seqs)
+
         out.retain_grad()
 
         # with torch.autograd.detect_anomaly():
@@ -74,19 +82,26 @@ def train():
         p_loss = 0
         epoch_sse = 0
 
-        for all_x, all_labels, all_label_mask, all_input_mask in loader:
+        for batch_val in loader:
 
-            xs = torch.split(all_x, bptt_steps, 1)
-            ls = torch.split(all_labels, bptt_steps, 1)
-            i_ms = torch.split(all_input_mask, bptt_steps, 1)
-            l_ms = torch.split(all_label_mask, bptt_steps, 1)
-            model.reset()
-            for i, (x, labels, input_mask, label_mask) in enumerate(
-                zip(xs, ls, i_ms, l_ms)
-            ):
-                # for x, labels, label_mask, input_mask in loader:
+            if not with_texts:
+                all_x, all_labels, all_label_mask, all_input_mask = batch_val
+                xs = torch.split(all_x, bptt_steps, 1)
+                ls = torch.split(all_labels, bptt_steps, 1)
+                i_ms = torch.split(all_input_mask, bptt_steps, 1)
+                l_ms = torch.split(all_label_mask, bptt_steps, 1)
+                model.reset()
+                for i, (x, labels, input_mask, label_mask) in enumerate(
+                    zip(xs, ls, i_ms, l_ms)
+                ):
+                    # for x, labels, label_mask, input_mask in loader:
+                    prob_loss, e_t_loss, sse = _single_step(
+                        x, labels, label_mask, input_mask
+                    )
+            else:
+                all_x, all_labels, all_label_mask, all_input_mask, all_seqs = batch_val
                 prob_loss, e_t_loss, sse = _single_step(
-                    x, labels, label_mask, input_mask
+                    all_x, all_labels, all_label_mask, all_input_mask, all_seqs
                 )
                 e_loss += e_t_loss
                 p_loss += prob_loss
@@ -109,4 +124,4 @@ def train():
 
 
 if __name__ == "__main__":
-    train()
+    fire.Fire(train)
