05fac9ea41f8513834cabd03ecfc2ca60b4f72f2
Ishaan Kumar <ffishaan@gmail.com>
2021-03-28 17:33:15 -0400

    train script cleanup

--- train.py
+++ train.py
@@ -1,146 +1,78 @@
+import argparse
 import os
-import shutil
-import sys
 import time
 
-import matplotlib.pyplot as plt
 import numpy as np
 import torch
-from matplotlib.lines import Line2D
-from torch.nn.functional import binary_cross_entropy, binary_cross_entropy_with_logits
+from torch.nn.functional import binary_cross_entropy_with_logits
 from torch.nn.utils import clip_grad_norm_
 from tqdm import tqdm
 
 from constants import BATCH_FIRST
 from data import get_loader
 from models.prediction import PredModel
+from utils.helper import surgery as surgery_fn
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 root = ""  # "/tmp/" if torch.cuda.is_available() else ""
-
 
 torch.manual_seed(0)
 np.random.seed(0)
 
 
-def plot_grad_flow(named_parameters):
-    """Plots the gradients flowing through different layers in the net during training.
-    Can be used for checking for possible gradient vanishing / exploding problems.
-
-    Usage: Plug this function in Trainer class after loss.backwards() as
-    "plot_grad_flow(self.model.named_parameters())" to visualize the gradient flow"""
-    ave_grads = []
-    max_grads = []
-    layers = []
-    plt.figure(figsize=((10, 20)))
-    for n, p in named_parameters:
-        if (p.requires_grad) and ("bias" not in n):
-            layers.append(n)
-            ave_grads.append(p.grad.abs().mean())
-            max_grads.append(p.grad.abs().max())
-    plt.bar(np.arange(len(max_grads)), max_grads, alpha=0.1, lw=1, color="c")
-    plt.bar(np.arange(len(max_grads)), ave_grads, alpha=0.1, lw=1, color="b")
-    plt.hlines(0, 0, len(ave_grads) + 1, lw=2, color="k")
-    plt.xticks(range(0, len(ave_grads), 1), layers, rotation="vertical")
-    plt.xlim(left=0, right=len(ave_grads))
-    plt.ylim(bottom=-0.001, top=0.02)  # zoom in on the lower gradient regions
-    plt.xlabel("Layers")
-    plt.ylabel("average gradient")
-    plt.title("Gradient flow")
-    plt.grid(True)
-    plt.legend(
-        [
-            Line2D([0], [0], color="c", lw=4),
-            Line2D([0], [0], color="b", lw=4),
-            Line2D([0], [0], color="k", lw=4),
-        ],
-        ["max-gradient", "mean-gradient", "zero-gradient"],
-    )
-    plt.savefig("grad.png")
-    plt.close()
-
-
-@torch.no_grad()
-def surgery(old_model, new_model):
-    old_lstm_layer = old_model.lstm.layers[0]
-    new_model.lstm.layers[0].weight_ih[...] = old_lstm_layer.weight_ih_l0[...]
-    new_model.lstm.layers[0].weight_hh[...] = old_lstm_layer.weight_hh_l0[...]
-    new_model.lstm.layers[0].bias_ih[...] = old_lstm_layer.bias_ih_l0[...]
-    new_model.lstm.layers[0].bias_hh[...] = old_lstm_layer.bias_hh_l0[...]
-    new_model.lstm.input_proj[0].weight[...] = old_model.lstm.input_proj[0].weight[...]
-    new_model.lstm.input_proj[0].bias[...] = old_model.lstm.input_proj[0].bias[...]
-    new_model.lstm.final_projs[0].weight[...] = old_model.lstm.final_projs[0].weight[
-        ...
-    ]
-    new_model.lstm.final_projs[0].bias[...] = old_model.lstm.final_projs[0].bias[...]
-    new_model.lstm.bias[...] = old_model.lstm.bias[...]
-    return new_model
-
-
-def train(second_stage: bool = False):
+def train(second_stage: bool = False, surgery: str = None, load_path: str = None):
     batch_size = 64
     lr = 1e-3
-    with_texts = True if second_stage else False
-    # if with_texts:
-    #     new_model = PredModel(
-    #         3, 1, 20, batch_size=batch_size, hidden_dim=900, with_texts=with_texts
-    #     ).to(device)
-    #     old_model = PredModel(
-    #         3, 1, 20, batch_size=batch_size, hidden_dim=900, with_texts=False
-    #     ).to(device)
-    #     save_dir = "runs/64_0.001_1616506139.0575218" + "/model.pt"
-    #     # save_dir = "beluga/64_0.001_1616506139.0575218.pt"
-    #     state = torch.load(open(save_dir, "rb"), map_location=torch.device(device))
-    #     old_model.load_state_dict(state["model"])
-    #     model = surgery(old_model, new_model)
-    #     del old_model
     model = PredModel(
-        3, 1, 20, batch_size=batch_size, hidden_dim=900, with_texts=with_texts
+        3, 1, 20, batch_size=batch_size, hidden_dim=900, with_texts=second_stage
     ).to(device)
-    # model = (model)
-    optim = torch.optim.AdamW(model.parameters(), lr=lr)
-    # save_dir = "runs/64_0.0001_1616797921.399419"
-    save_dir = "runs/64_0.001_1616852698.2713563"
-    state = torch.load(
-        open(save_dir + "/model.pt", "rb"), map_location=torch.device(device)
-    )
-    model.load_state_dict(state["model"])
-    optim.load_state_dict(state["optim"])
-    start_epoch = state["epoch"]
-    # start_epoch = 0
-    # optim = torch.optim.RMSprop(
-    #     model.parameters(), lr=lr, alpha=0.9, momentum=0.95, eps=1e-4
-    # )
-    loader, dataset = get_loader(
-        root + "data/strokes-py3.npy", batch_size, with_texts=with_texts
-    )
-    # loader, dataset = get_loader("data/strokes-py3.npy", batch_size)
-    num_epochs = 100
-    # save_dir = f"{root}runs/{batch_size}_{lr}_{time.time()}"
-    # os.makedirs(save_dir)
-    bptt_steps = 100
-    # model.generate_with_seq(1, device, torch.randn(1, 17, 58))
 
-    is_scaling = False
+    if surgery and second_stage:
+        old_model = PredModel(
+            3, 1, 20, batch_size=batch_size, hidden_dim=900, with_texts=False
+        ).to(device)
+        save_dir = os.path.join(surgery, "model.pt")
+        state = torch.load(open(save_dir, "rb"), map_location=torch.device(device))
+        old_model.load_state_dict(state["model"])
+        model = surgery_fn(old_model, model)
+        del old_model
+
+    optim = torch.optim.AdamW(model.parameters(), lr=lr)
+    start_epoch = 0
+
+    if load_path is not None:
+        state = torch.load(
+            open(os.path.join(load_path, "model.pt"), "rb"),
+            map_location=torch.device(device),
+        )
+        model.load_state_dict(state["model"])
+        optim.load_state_dict(state["optim"])
+        start_epoch = state["epoch"]
+    loader, _ = get_loader(
+        os.path.join(root, "data/strokes-py3.npy"), batch_size, with_texts=second_stage
+    )
+    num_epochs = 100
+    save_dir = f"{root}runs/{batch_size}_{lr}_{time.time()}"
+    os.makedirs(save_dir)
+
+    is_scaling = True
     scaler = torch.cuda.amp.GradScaler(enabled=is_scaling)
 
-    def _single_step(
-        x, labels, label_mask, input_mask, seqs=None, seq_mask=None, alpha=0
-    ):
+    def _single_step(x, label_mask, input_mask, seqs=None):
         x = x.to(device)
         labels = x[:, :, 0]
-        # labels = labels.to(device)
         label_mask = label_mask.to(device)
         input_mask = input_mask.to(device)
         if second_stage:
             seqs = seqs.to(device)
-            seq_mask = seq_mask.to(device)
         with torch.cuda.amp.autocast(enabled=is_scaling):
-            out = model(x, seqs, seq_mask=seq_mask)
-            # out = out[:, :-1, :]
+            # forward pass
+            out = model(x, seqs)
+
+            # retain grads for clipping
             out.retain_grad()
 
-            # with torch.autograd.detect_anomaly():
+            # compute likelihood and sse metric
             prob_t, e_t, sse = model.infer(
                 out, x[:, :, 1:], input_mask, label_mask
             )  # shape = (b,s)
@@ -148,29 +80,24 @@ def train(second_stage: bool = False):
             # calculate loss
             prob_loss = -prob_t.sum()
 
-            # one_mask = labels[label_mask] == 1
-            # assert torch.isfinite(e_t).all()
-            # assert torch.isfinite(labels).all()
             e_t_loss = binary_cross_entropy_with_logits(
                 input=e_t[input_mask], target=labels[label_mask], reduction="none"
             )
-            # e_t_loss[one_mask] *= dataset.w_1
-            # e_t_loss[torch.logical_not(one_mask)] *= 1 - dataset.w_1
-
             e_t_loss = e_t_loss.sum()
 
+            # divide seq loss by number of batches
             loss = (prob_loss + e_t_loss) / len(label_mask.sum(1) > 0)
 
         optim.zero_grad()
-        # loss.backward()
         scaler.scale(loss).backward()
         scaler.unscale_(optim)
+
         # clip w grad and output grad
         out.grad.clamp_(-100, 100)
         clip_grad_norm_(model.parameters(), 10)
+
         # plot_grad_flow(model.named_parameters())
 
-        # optim.step()
         scaler.step(optim)
         scaler.update()
 
@@ -183,74 +110,56 @@ def train(second_stage: bool = False):
         p_loss = 0
         epoch_sse = 0
 
-        for bi, batch_val in enumerate(loader):
-
+        for batch_val in loader:
             model.reset()
-            # if not with_texts:
-            # all_x, all_labels, all_label_mask, all_input_mask = batch_val
-            # xs = torch.split(all_x, bptt_steps, 1)
-            # ls = torch.split(all_labels, bptt_steps, 1)
-            # i_ms = torch.split(all_input_mask, bptt_steps, 1)
-            # l_ms = torch.split(all_label_mask, bptt_steps, 1)
-            # for i, (x, labels, input_mask, label_mask) in enumerate(
-            #     zip(xs, ls, i_ms, l_ms)
-            # ):
-            #     # for x, labels, label_mask, input_mask in loader:
-            #     if x.shape[1] == 1:
-            #         continue
-            #     prob_loss, e_t_loss, sse = _single_step(
-            #         x, labels, label_mask, input_mask
-            #     )
-            # else:
-            if not with_texts:
-                all_x, all_labels, all_label_mask, all_input_mask = batch_val
-                seqs = None
-                seq_mask = None
-            else:
-                (
-                    all_x,
-                    all_labels,
-                    all_label_mask,
-                    all_input_mask,
-                    seqs,
-                    seq_mask,
-                ) = batch_val
-
-            # try:
+            all_x, all_label_mask, all_input_mask, seqs = batch_val
             prob_loss, e_t_loss, sse = _single_step(
                 all_x,
-                all_labels,
                 all_label_mask,
                 all_input_mask,
                 seqs=seqs,
-                seq_mask=seq_mask,
-                alpha=epoch // num_epochs,
             )
-            # except Exception as e:
-            #     print(bi)
-            #     raise e
 
             e_loss += e_t_loss / batch_size
             p_loss += prob_loss / batch_size
             epoch_sse += sse
 
         len_batches = len(loader)
-        # if not with_texts:
-        # len_batches *= len(xs)
         e_loss /= len_batches
         p_loss /= len_batches
         epoch_loss = e_loss + p_loss
         print(
             f"Epoch {epoch}: Loss {epoch_loss}({e_loss}+{p_loss}) | SSE: {epoch_sse / len_batches}"
         )
+
         # checkpoint weights
         torch.save(
             {"model": model.state_dict(), "optim": optim.state_dict(), "epoch": epoch},
             open(save_dir + "/model.pt", "wb"),
         )
 
-    # shutil.move(save_dir, save_dir[len(root) :])
-
 
 if __name__ == "__main__":
-    train(eval(sys.argv[1]))
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--second",
+        action="store_true",
+        default=False,
+        help="Set this flag to train a model conditioned on the sequence text",
+    )
+    parser.add_argument(
+        "--surgery",
+        type=str,
+        default=None,
+        help="Use a 1st stage model's weights to initialize 2nd stage model's weights."
+        + " Specify the path of 1st stage model in this argument. "
+        + "Use only for 2nd stage models. This is ignored for 1st stage models.",
+    )
+    parser.add_argument(
+        "--load_path",
+        type=str,
+        default=None,
+        help="Directory path to restore the model and optimizer state from.",
+    )
+    args = parser.parse_args()
+    train(args.second, args.surgery, args.load_path)
--- utils/helper.py
+++ utils/helper.py
@@ -1,5 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
 import torch
 import torch.nn as nn
+from matplotlib.lines import Line2D
 from torch.functional import Tensor
 
 
@@ -40,3 +43,57 @@ def sse_x(target, w, means):
     return nn.functional.mse_loss(target[:, 0], pred[:, 0]) + nn.functional.mse_loss(
         target[:, 1], pred[:, 1]
     )  # compute error for each dim separately
+
+
+def plot_grad_flow(named_parameters):
+    """Plots the gradients flowing through different layers in the net during training.
+    Can be used for checking for possible gradient vanishing / exploding problems.
+
+    Usage: Plug this function in Trainer class after loss.backwards() as
+    "plot_grad_flow(self.model.named_parameters())" to visualize the gradient flow"""
+    ave_grads = []
+    max_grads = []
+    layers = []
+    plt.figure(figsize=((10, 20)))
+    for n, p in named_parameters:
+        if (p.requires_grad) and ("bias" not in n):
+            layers.append(n)
+            ave_grads.append(p.grad.abs().mean())
+            max_grads.append(p.grad.abs().max())
+    plt.bar(np.arange(len(max_grads)), max_grads, alpha=0.1, lw=1, color="c")
+    plt.bar(np.arange(len(max_grads)), ave_grads, alpha=0.1, lw=1, color="b")
+    plt.hlines(0, 0, len(ave_grads) + 1, lw=2, color="k")
+    plt.xticks(range(0, len(ave_grads), 1), layers, rotation="vertical")
+    plt.xlim(left=0, right=len(ave_grads))
+    plt.ylim(bottom=-0.001, top=0.02)  # zoom in on the lower gradient regions
+    plt.xlabel("Layers")
+    plt.ylabel("average gradient")
+    plt.title("Gradient flow")
+    plt.grid(True)
+    plt.legend(
+        [
+            Line2D([0], [0], color="c", lw=4),
+            Line2D([0], [0], color="b", lw=4),
+            Line2D([0], [0], color="k", lw=4),
+        ],
+        ["max-gradient", "mean-gradient", "zero-gradient"],
+    )
+    plt.savefig("grad.png")
+    plt.close()
+
+
+@torch.no_grad()
+def surgery(old_model, new_model):
+    old_lstm_layer = old_model.lstm.layers[0]
+    new_model.lstm.layers[0].weight_ih[...] = old_lstm_layer.weight_ih_l0[...]
+    new_model.lstm.layers[0].weight_hh[...] = old_lstm_layer.weight_hh_l0[...]
+    new_model.lstm.layers[0].bias_ih[...] = old_lstm_layer.bias_ih_l0[...]
+    new_model.lstm.layers[0].bias_hh[...] = old_lstm_layer.bias_hh_l0[...]
+    new_model.lstm.input_proj[0].weight[...] = old_model.lstm.input_proj[0].weight[...]
+    new_model.lstm.input_proj[0].bias[...] = old_model.lstm.input_proj[0].bias[...]
+    new_model.lstm.final_projs[0].weight[...] = old_model.lstm.final_projs[0].weight[
+        ...
+    ]
+    new_model.lstm.final_projs[0].bias[...] = old_model.lstm.final_projs[0].bias[...]
+    new_model.lstm.bias[...] = old_model.lstm.bias[...]
+    return new_model
